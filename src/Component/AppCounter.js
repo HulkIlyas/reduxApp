6e15163b5a8b7ce70b3c83bc9cb4baa44aa6ebdf
Ilyas <46636662+HulkIlyas@users.noreply.github.com>
2019-12-25 00:22:49 -0800

    Dispatch branch

--- src/Component/AppCounter.js
+++ src/Component/AppCounter.js
@@ -1,12 +1,13 @@
 import { createAppContainer } from "react-navigation"
 import { createStackNavigator } from "react-navigation-stack";
-import { CountCounter, PersonScreen, UseSelectorScreen } from "screen";
+import { CountCounter, PersonScreen, UseSelectorScreen, UseDispatchScreen } from "screen";
 
 
 const Main = createStackNavigator({
     counter: CountCounter,
     Person: PersonScreen,
-    UseSelectorScreen
+    UseSelectorScreen,
+    UseDispatchScreen
 }, {
     initialRouteName: "counter"
 })
--- src/Screen/index.js
+++ src/Screen/index.js
@@ -1,3 +1,4 @@
 export * from "./CountCounter"
 export * from "./PersonScreen"
 export * from "./UseSelectorScreen"
+export * from "./UseDispatchScreen"
